17ceb700494cc62424357ad118eddcd94b2dedd9
Brian Cain <bcain@hashicorp.com>
2018-09-17 12:32:23 -0700

    Move around player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+  [SerializeField]
+  float playerSpeed;
+
+  private Rigidbody rb;
+
   // Use this for initialization
   void Start () {
+    rb = GetComponent<Rigidbody>();
+    playerSpeed = 5;
   }
   // Update is called once per frame
   void Update () {
+  }
+
+  void FixedUpdate() {
+    float moveHorizontal = Input.GetAxis ("Horizontal");
+    float moveVertical = Input.GetAxis ("Vertical");
+
+    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+    rb.velocity =  movement * playerSpeed;
   }
 }
