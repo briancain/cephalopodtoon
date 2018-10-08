c6efa32985db684907244ecde451d3c1006514bf
Brian Cain <bcain@hashicorp.com>
2018-10-08 12:27:19 -0700

    Halt ink splats when firing

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Fire : MonoBehaviour {
 
+  private Rigidbody rb;
   // Use this for initialization
   void Start () {
+    rb = GetComponent<Rigidbody>();
   }
   // Update is called once per frame
   void Update () {
+  }
+
+  void OnCollisionEnter(Collision collider) {
+    if (collider.gameObject.CompareTag("Background")) {
+      rb.velocity = new Vector3(0,0,0);
+    }
   }
 }
